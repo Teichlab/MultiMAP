6ea0c1e07c5d8fe152ab88bcfe0e23ab69504866
ktpolanski <krzysztof_polanski@o2.pl>
2021-06-07 16:40:34 +0100

    try old mock again in case it helps

--- docs/conf.py
+++ docs/conf.py
@@ -13,10 +13,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
-autodoc_mock_imports = ['anndata','scanpy','numpy','scipy','numba','scipy.optimize',
-						'sklearn.neighbors','sklearn.metrics','warnings','scipy.sparse',
-						'locale','sklearn.utils','annoy','faiss','scipy.sparse.csgraph',
-						'sklearn.metrics','FeatureHasher','sklearn.manifold','sklearn']
+autodoc_mock_imports = ['MultiMAP.matrix','anndata','scanpy','numpy','scipy']
 
 # -- Project information -----------------------------------------------------
 
