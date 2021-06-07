3b7a764896557b3f458db587bf44c395d52e9144
ktpolanski <krzysztof_polanski@o2.pl>
2021-06-07 16:43:03 +0100

    okay it's not the mocks

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,10 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
-autodoc_mock_imports = ['MultiMAP.matrix','anndata','scanpy','numpy','scipy']
+autodoc_mock_imports = ['anndata','scanpy','numpy','scipy','numba','scipy.optimize',
+						'sklearn.neighbors','sklearn.metrics','warnings','scipy.sparse',
+						'locale','sklearn.utils','annoy','faiss','scipy.sparse.csgraph',
+						'sklearn.metrics','FeatureHasher','sklearn.manifold','sklearn']
 
 # -- Project information -----------------------------------------------------
 
